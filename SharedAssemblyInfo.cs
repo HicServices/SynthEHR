420fe050980f91e1b19adc4bd6f8c44ebd850b92
Thomas Nind <TZNind@dundee.ac.uk>
2019-05-10 10:17:37 +0100

    RDMPDEV-1408 Version bump

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.1.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: AssemblyInformationalVersion("0.0.1.1")]
