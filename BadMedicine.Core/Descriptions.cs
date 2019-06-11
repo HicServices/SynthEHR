bb8a9c631f6f2730a08748035b5fda6215fc0517
Thomas Nind <TZNind@dundee.ac.uk>
2019-06-11 12:30:11 +0100

    RDMPDEV-1440 Made Datasets.doc.xml an embedded resource and added a manager class Descriptions to provide runtime metadata about dataset columns (by xml parsing).

---
+++ BadMedicine.Core/Descriptions.cs
@@ -0,0 +1,108 @@
+﻿using BadMedicine.Datasets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BadMedicine
+{
+    /// <summary>
+    /// Provides descriptions for what is in each dataset column (at runtime).  This is the same information that appears in xmldocs.
+    /// </summary>
+    public class Descriptions
+    {
+        private XDocument _doc;
+        private XNamespace _ns;
+
+        /// <summary>
+        /// Creates new instance ready to provide column descriptions from embedded resource file
+        /// </summary>
+        public Descriptions()
+        {
+            using(var stream = typeof(Descriptions).Assembly.GetManifestResourceStream("BadMedicine.Datasets.doc.xml"))
+            {
+                _doc = XDocument.Load(stream);
+                _ns = _doc.Root.GetDefaultNamespace();
+            }
+        }
+
+        /// <summary>
+        /// Returns all columns with descriptions in the dataset (does not include Common fields e.g. chi)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IEnumerable<KeyValuePair<string,string>> GetAll<T>()
+        {
+            return GetAll(typeof(T).Name);
+        }
+
+        /// <summary>
+        /// Returns all columns with descriptions in the dataset (does not include Common fields e.g. chi - unless you pass "Common" for <paramref name="dataset"/>)
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<KeyValuePair<string,string>> GetAll(string dataset)
+        {
+            var ds = _doc.Root?.Element(_ns + dataset);
+            
+            if(ds == null)
+                yield break;
+
+            foreach(var x in ds.Elements(_ns + "Field"))
+                yield return new KeyValuePair<string,string>(x.Attribute(_ns + "name").Value,x.Value);
+        }
+        
+        
+        /// <summary>
+        /// Returns the description of the dataset <typeparamref name="T"/> (or null if no metadata exists)
+        /// </summary>
+        /// <typeparam name="T">Dataset you want the description of</typeparam>
+        /// <returns></returns>
+        public string Get(string dataset)
+        {
+            return _doc.Root?.Element(_ns + dataset)?.Element(_ns + "summary")?.Value;
+        }
+            
+
+        /// <summary>
+        /// Returns the description of the dataset <typeparamref name="T"/> (or null if no metadata exists)
+        /// </summary>
+        /// <typeparam name="T">Dataset you want the description of</typeparam>
+        /// <returns></returns>
+        public string Get<T>() where T:IDataGenerator
+        {
+            return Get(typeof(T).Name);
+        }
+
+        /// <summary>
+        /// Returns the description of the column <paramref name="field"/> in dataset <typeparamref name="T"/> (or null if no metadata exists)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public string Get<T>(string field) where T:IDataGenerator
+        {            
+            return Get(typeof(T).Name,field);
+        }
+
+        /// <summary>
+        /// Returns the description of the column <paramref name="field"/> in the <paramref name="dataset"/> (or null if no metadata exists)
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public string Get(string dataset,string field)
+        {   
+            return 
+                //return from the dataset fields
+                _doc.Root.Element(_ns + dataset)?.Elements(_ns + "Field")?
+                
+                //the one whose name attribute matches
+                .SingleOrDefault(e=>string.Equals(e.Attribute(_ns + "name")?.Value,field,StringComparison.CurrentCultureIgnoreCase))?.Value
+                
+                //or from Common if it's not in the dataset.
+                ?? (dataset != "Common" ? Get("Common",field):null); //null if we are already trying Common
+
+        }
+    }
+}
---
+++ BadMedicineTests/DescriptionsTests.cs
@@ -0,0 +1,88 @@
+﻿using BadMedicine;
+using BadMedicine.Datasets;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadMedicineTests
+{
+    class DescriptionsTests
+    {
+        [Test]
+        public void Test_GetDescription_Dataset()
+        {
+            var desc = new Descriptions();
+
+            StringAssert.Contains("Tayside and Fife labs biochemistry data",desc.Get<Biochemistry>());
+        }
+
+        [Test]
+        public void Test_GetDescription_Field()
+        {
+            var desc = new Descriptions();
+
+            StringAssert.Contains("Health Board code",desc.Get<Biochemistry>("Healthboard"));
+            StringAssert.Contains("Health Board code",desc.Get("Biochemistry","Healthboard"));
+        }
+
+        [Test]
+        public void Test_GetDescription_FieldNotFound()
+        {
+            var desc = new Descriptions();
+
+            Assert.IsNull(desc.Get<Biochemistry>("ZappyZappyZappy"));
+            Assert.IsNull(desc.Get("Biochemistry","ZappyZappyZappy"));
+        }
+        [Test]
+        public void Test_GetDescription_FieldNotFoundAndDatasetNotFound()
+        {
+            var desc = new Descriptions();
+            Assert.IsNull(desc.Get("Happy","ZappyZappyZappy"));
+
+            Assert.IsEmpty(desc.GetAll("Happy"));
+        }
+
+        /// <summary>
+        /// This tests when the field is in CommonFields not under the dataset
+        /// </summary>
+        [Test]
+        public void Test_GetDescription_ChiField()
+        {
+            var desc = new Descriptions();
+            StringAssert.Contains("Community Health Index (CHI) number is a unique personal identifier ",desc.Get<Biochemistry>("chi"));
+            StringAssert.Contains("Community Health Index (CHI) number is a unique personal identifier ",desc.Get("Biochemistry","chi"));
+        }
+
+        [Test]
+        public void Test_GetAllDescriptions_Biochemistry()
+        {
+            var desc = new Descriptions();
+
+            Assert.GreaterOrEqual(desc.GetAll<Biochemistry>().ToArray().Length,5);
+            Assert.GreaterOrEqual(desc.GetAll("Biochemistry").ToArray().Length,5);
+            Assert.GreaterOrEqual(desc.GetAll("Common").ToArray().Length,1);
+
+
+            foreach(var kvp in desc.GetAll<Biochemistry>())
+            {
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(kvp.Key), "Found null key");
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(kvp.Value),"Found null description for" + kvp.Key);
+            }
+        }
+
+        [Test]
+        public void Test_GetAllDescriptions_AllDatasets()
+        {
+            var desc = new Descriptions();
+
+            var factory = new DataGeneratorFactory();
+            foreach(var g in factory.GetAvailableGenerators())
+            {
+                Assert.IsNotNull(desc.Get(g.Name),"Dataset '{0}' did not have a summary tag",g.Name);
+                Assert.IsNotEmpty(desc.GetAll(g.Name),"Dataset '{0}' did not have at least one column description",g.Name);
+            }
+        }
+    }
+}
