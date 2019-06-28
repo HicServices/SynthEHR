6a1621961b2eb8b7d0944cb6ee2efab6ac31a001
Thomas Nind <TZNind@dundee.ac.uk>
2019-06-28 10:43:55 +0100

    Bumped version to 0.1.3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2")]
-[assembly: AssemblyInformationalVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.3")]
+[assembly: AssemblyFileVersion("0.1.3")]
+[assembly: AssemblyInformationalVersion("0.1.3")]
