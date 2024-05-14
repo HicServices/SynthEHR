c6d961ddb4c8b7b6d49db3075f5d14433c48960b
James Friel <JFriel001@dundee.ac.uk>
2024-05-14 10:54:48 +0100

    update shared assembly

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
-[assembly: AssemblyProduct("Bad Medicine")]
+[assembly: AssemblyProduct("SynthEHR")]
 [assembly: AssemblyCopyright("Copyright (c) 2018 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
-[assembly: AssemblyInformationalVersion("1.2.1")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
